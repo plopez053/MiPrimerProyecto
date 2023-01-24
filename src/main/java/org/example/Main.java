c2ed27a6786c3d9069e77c13904443b6293ea08a
plopez053 <plopez053@ikasle.ehu.eus>
2023-01-24 17:36:28 +0100

    Otro cambio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Este es un nuevo mensaje");
         System.out.println("Version definitiva!");
     }
 }
