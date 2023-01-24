d791b1b3ca5960689b4cf30eb991c33cab881f6c
plopez053 <plopez053@ikasle.ehu.eus>
2023-01-24 17:21:27 +0100

    Version definitiva

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Otra version!");
+        System.out.println("Version definitiva!");
     }
 }
